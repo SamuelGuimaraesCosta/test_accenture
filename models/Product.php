fb7da3d84d7e299efdd8acedda4fd8f6df2bccf5
Samuel Guimarães <samgcosta@hotmail.com>
2023-12-20 19:40:45 -0300

    added product view

---
+++ models/Product.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property string $name
+ * @property float $price
+ * @property string|null $description
+ * @property string $create_date
+ * @property string|null $update_date
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['price'], 'number'],
+            [['create_date', 'update_date'], 'safe'],
+            [['name'], 'string', 'max' => 400],
+            [['description'], 'string', 'max' => 500],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'price' => Yii::t('app', 'Price'),
+            'description' => Yii::t('app', 'Description'),
+            'create_date' => Yii::t('app', 'Create Date'),
+            'update_date' => Yii::t('app', 'Update Date'),
+        ];
+    }
+}
